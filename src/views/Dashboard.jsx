507b882679c430f96ab7e9dea8d8a7253b935ede
Ronier Ramos <front.menudeldia@gastrocloud.io>
2023-03-15 03:55:58 +0100

    add user info to dashboard component

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -3,18 +3,19 @@ import { Navbar } from '../components/Navbar';
 import {
     Outlet,
 } from 'react-router-dom';
+import { AuthContext } from "../context/auth.context";
+import { useContext } from "react";
 
 export default function Home() {
-
-
+    const { user, logout } = useContext(AuthContext)
     return (
         <div>
-            <Navbar />
+            <Navbar user={user.name} logout={logout} />
             <div style={{
                 display: 'flex',
                 width: '100%'
             }}>
-                <Drawer />
+                <Drawer user={user.name} />
                 <Outlet />
             </div>
         </div>
