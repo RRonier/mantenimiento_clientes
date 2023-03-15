9488d95437d7657eaf0d5d09d8b3399d61add520
Ronier Ramos <front.menudeldia@gastrocloud.io>
2023-03-15 03:53:52 +0100

    add auth context

---
+++ src/context/auth.context.jsx
@@ -0,0 +1,44 @@
+
+import { createContext, useState } from "react"
+import { loginService, signUpService } from "../services/auth.service"
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState({
+        name: ""
+    });
+    const [isLogged, setIsLogged] = useState(false)
+
+    const loginContext = async (username, password) => {
+        // setLoginPending(true)
+        // setLoginSuccess(false)
+        // setLoginError(null)
+        try {
+            let { data } = await loginService(username, password)
+            setUser({ name: data.username })
+            setIsLogged(true)
+            localStorage.setItem('username', data.username)
+            localStorage.setItem('userid', data.userid)
+            localStorage.setItem('token', data.token)
+            localStorage.setItem('expiration', data.expiration)
+            // setLoginSuccess(true)
+        } catch (error) {
+            // setLoginError(error)
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem('username')
+        localStorage.removeItem('userid')
+        localStorage.removeItem('token')
+        localStorage.removeItem('expiration')
+        setIsLogged(false)
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loginContext, logout, isLogged }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
