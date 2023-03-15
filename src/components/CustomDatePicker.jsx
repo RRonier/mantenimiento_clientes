978cb64114b12403077fc47ec77445f60c99fb0e
Ronier Ramos <front.menudeldia@gastrocloud.io>
2023-03-15 03:57:18 +0100

    add custom datepicker component

---
+++ src/components/CustomDatePicker.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+
+
+export default function CustomDatePicker(props) {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer
+                sx={{
+                    paddingTop: 0,
+                    width: '100%',
+                }} components={['DatePicker']}>
+                <DatePicker
+                    {...props}
+                    sx={{
+                        width: '100%',
+                    }} {...props} />
+            </DemoContainer>
+        </LocalizationProvider>
+    );
+}
