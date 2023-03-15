a31bd9a765f23a629dd4d32273a6eeb2780d06bb
Ronier Ramos <front.menudeldia@gastrocloud.io>
2023-03-15 03:52:12 +0100

    add protection to routes

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom"
+import { AuthContext } from '../context/auth.context';
+import { useContext } from 'react';
+
+export const ProtectedRoute = ({ children }) => {
+    const { isLogged } = useContext(AuthContext)
+    if (!isLogged) {
+        return <Navigate to="/" />
+    }
+    return children
+}
